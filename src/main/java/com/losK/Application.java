253c5533e17886f8e8ac9788b5f317d17afafac0
Vitalij <kontrabasista@wp.pl>
2017-03-01 19:19:29 +0100

    Initial commit

---
+++ src/main/java/com/losK/Application.java
@@ -0,0 +1,10 @@
+package com.losK;
+
+/**
+ * Created by m.losK on 2017-03-01.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
