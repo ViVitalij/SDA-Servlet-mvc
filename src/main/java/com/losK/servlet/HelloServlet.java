c740f035865492c60b8d4fe87b7bd5a115f806ef
Vitalij <kontrabasista@wp.pl>
2017-03-07 20:53:52 +0100

    Added doPost method with dependency on REST-Client(myPostman)

--- src/main/java/com/losK/servlet/HelloServlet.java
+++ src/main/java/com/losK/servlet/HelloServlet.java
@@ -5,13 +5,16 @@ import com.losK.controller.CookieController;
 import com.losK.controller.LoginController;
 import com.losK.controller.UserController;
 import org.apache.commons.lang3.StringUtils;
+import pl.sda.file.FileOperations;
+import pl.sda.file.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,5 +45,23 @@ public class HelloServlet extends HttpServlet {
             controller = controllerMap.get("default");
         }
         controller.handleGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+        String body = reader.readLine();
+        String[] split = body.split(" ");
+
+        User user = new User();
+        user.setFirstName(split[0]);
+        user.setLastName(split[1]);
+        user.setAge(new Integer(split[2]));
+
+        File file = new File("C:\\Users\\RENT\\Desktop\\test.txt");
+        FileOperations.addUserToFile(user, file);
+
+        resp.getWriter().write("Everything OK");
+        resp.setStatus(201);
     }
 }
